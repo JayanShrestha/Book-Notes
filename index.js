5b56f2e1b6e207e14ae6cb58d09df9d575953a95
Jayan Shrestha <jayanshr44@gmail.com>
2025-12-18 18:08:51 +1100

    database connection was achieved and added scroll animation

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import pg from "pg";
+import env from "dotenv";
 
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
+env.config();
 
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+const db = new pg.Client({
+        user:process.env.PG_USER,
+        host:process.env.PG_HOST,
+        database:process.env.PG_DATABASE,
+        password:process.env.PG_PASSWORD,
+        port:process.env.PG_PORT,
+});
+
+db.connect();
 
 const booksData = [
         { title: "The Fresh Startup", author: "Jane Doe", price: 24.99 },
@@ -18,7 +32,14 @@ const booksData = [
         { title: "Yellow Brick Road to Success", author: "Michael Brown", price: 15.99 }
     ];
 
-app.get("/", (req,res)=>{
+app.get("/",async (req,res)=>{
+        try{
+                const books = await db.query("SELECT * from books");
+                console.log(books.rows);
+        }
+        catch(err){
+                console.log("The data from books table wasn't retrieved",err);
+        }
 
 
 res.render("index.ejs",{pageTitle: "Book Notes Demo | Favourite Books",
---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+window.sr= ScrollReveal();//scroll animation for html element
+sr.reveal('.hero-content',{
+    duration:2000,
+    distance:"50px",
+    origin:'left'
+});
+
+sr.reveal('.hero-image',{
+    duration:2000,
+    distance:"50px",
+    origin:'top'
+});
+
+sr.reveal('header',{
+    duration:2000,
+    distance:"50px",
+    origin:'bottom'
+});
